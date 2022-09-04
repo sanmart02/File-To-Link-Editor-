0fa5ccf7819c6803d429dae7dd878b6c7663a7b4
jagan1857 <107793234+jagan1857@users.noreply.github.com>
2022-09-04 08:31:23 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -98,7 +98,7 @@ async def private_receive_handler(c: Client, m: Message):
         
         online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         
-        photo_xr="https://telegra.ph/file/3cd15a67ad7234c2945e7.jpg"
+        photo_xr="https://telegra.ph/file/e29e579b25c2cf0341baf.jpg"
         
         
 
